67a93b5d76951067caf9788ee697957ca344598a
Samir Das <samir.das1055@gmail.com>
2024-04-05 19:25:29 +0530

    Some code

---
+++ Array.py
@@ -0,0 +1,7 @@
+#Defining Arrays
+list_01=[1,2,3,4,5,6]
+print(list_01)
+list_02=["ruastf","ot","h","odtuykhjm,ydfjk,86,","udkjfg",78]
+print(list_02)
+thislist = list(list("apple", "banana", "cherry"),list(42326,6,754,987))
+print(thislist)
