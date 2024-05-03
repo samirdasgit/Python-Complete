ce72bdeb73e8463991f78cb4e9963b8f8f9be19d
Samir Das <samir.das1055@gmail.com>
2024-05-03 12:02:09 +0530

    Some code

---
+++ code10.py
@@ -0,0 +1,4 @@
+a=int(input("Enter A number:- "))
+b=int(input("Enter Another Number:- "))
+print(a + b)
+
---
+++ code11.py
@@ -0,0 +1,5 @@
+a = float(input("Enter Your Age :- "))
+if a < 18:
+  print("You Aren't Eligible For Voting Now!")
+else:
+  print("Yeah! You Are Eligible For Voting Now!")
