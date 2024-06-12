4e07f7ee7b76ef4c2262c57ae0ecd21978e8782e
Samir Das <samir.das1055@gmail.com>
2024-06-12 12:53:13 +0530

    3 Codes

---
+++ ListofSquares.py
@@ -0,0 +1,5 @@
+#squares = [x**2 for x in range(1, 11)]
+#print(squares)
+
+for i in range(1,11):
+  print(i*i)
---
+++ OddEven.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a no : "))
+if(a%2==1):
+  print("Odd")
+else:
+  print("Even")
