395e7d14c93e70d87eb57d76a086b28fb010190c
Samir Das <samir.das1055@gmail.com>
2025-08-25 15:55:04 +0530

    Code 5 to 7

---
+++ code5.py
@@ -0,0 +1,7 @@
+def sumTowNo(x,y):
+  sum = x + y
+  print("The sum of two numbers is:", sum)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sumTowNo(a,b)
---
+++ code6.py
@@ -0,0 +1,9 @@
+def adwiteya_func(a,b):
+  if a>b:
+    print(a," is largest")
+  else:
+    print (b," is largest")
+
+x=int(input("Enter a value:"))
+y=int(input("Enter a value:"))
+adwiteya_func(x,y)
